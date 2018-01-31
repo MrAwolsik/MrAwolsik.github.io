3bde82a5d193b5691c9961e0f6e775f5dddf8b47
Valentin <valentin.berard42@gmail.com>
2018-01-31 10:48:51 +0100

    Modification du formulaire de contact pour créer un input qui change la backgroud-color de la section contact grâce à un srcipt js

---
+++ js/script_contact.js
@@ -0,0 +1,22 @@
+
+var tab_color = [];
+
+function change_color() {
+    var value = document.getElementById("name").value;
+    console.log("Valeur de name", value);
+
+    if (value == "")
+    {
+        value = "white";
+    }
+    var element = document.getElementById("contact");
+    element.style.backgroundColor = value;
+    console.log("couleur du background : ", element.style.backgroundColor);
+
+    if (tab_color.indexOf(element.style.backgroundColor) == -1)
+    {
+        tab_color.push(element.style.backgroundColor);
+    }
+    console.log("Tableau des couleurs -> ", tab_color);
+}
+
